f972b281c299527862e45775dfdd3fc144572333
JR10 <hamdyjs@gmail.com>
2015-09-20 21:33:26 +0200

    [MTA] Add constructor

--- mta.js
+++ mta.js
@@ -1,1 +1,8 @@
 var request = require('request');
+
+function MTA(host, port, username, password) {
+  this.host = host || 'localhost';
+  this.port = port || 22005;
+  this.username = username;
+  this.password = password;
+}

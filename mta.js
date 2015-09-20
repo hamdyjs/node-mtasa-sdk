bb4b5877e1c9c24c6cbc5656e24d490592c78027
JR10 <hamdyjs@gmail.com>
2015-09-20 21:52:35 +0200

    [MTA] Make a function to throw errors on MTA.call

--- mta.js
+++ mta.js
@@ -6,3 +6,8 @@ function MTA(host, port, username, password) {
   this.username = username;
   this.password = password;
 }
+
+// Throw error for MTA.call with the msg
+function _callError(msg) {
+  throw new Error('Failed to call MTA.call: '+ msg);
+}
